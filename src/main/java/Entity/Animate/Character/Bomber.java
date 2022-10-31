d441c7eaf379b21e742c60d507d5a136c88de3b0
Zhao-Linux <dungbuit1k28@gmail.com>
2022-10-31 07:00:27 +0700

    improve place bombs

--- src/main/java/Entity/Animate/Character/Bomber.java
+++ src/main/java/Entity/Animate/Character/Bomber.java
@@ -41,13 +41,30 @@ public class Bomber extends Character {
 
     public void placeBombAt(int x, int y) {
         canPlace = true;
-        map.getBombs().forEach(bomb -> {
-            if (bomb.getTileX() == x && bomb.getTileY() == y) {
+        int bombX = 0;
+        int bombY = 0;
+        if (x % SCALED_SIZE < y % SCALED_SIZE) {
+            bombX = x / SCALED_SIZE;
+            if (y % SCALED_SIZE > SCALED_SIZE / 2) {
+                bombY = y / SCALED_SIZE + 1;
+            } else {
+                bombY = y / SCALED_SIZE;
+            }
+        } else {
+            bombY = y / SCALED_SIZE;
+            if (x % SCALED_SIZE > SCALED_SIZE / 2) {
+                bombX = x / SCALED_SIZE + 1;
+            } else {
+                bombX = x / SCALED_SIZE;
+            }
+        }
+        for (Bomb bomb: map.getBombs()) {
+            if (bomb.getTileX() == bombX && bomb.getTileY() == bombY) {
                 canPlace = false;
             }
-        });
-        if (map.getTile(x, y) instanceof Grass && map.getBombs().size() < Bomb.limit && canPlace) {
-            Bomb bomb = BombTexture.setBomb(x, y);
+        }
+        if (map.getTile(bombX, bombY) instanceof Grass && map.getBombs().size() < Bomb.limit && canPlace) {
+            Bomb bomb = BombTexture.setBomb(bombX, bombY);
             map.getBombs().add(bomb);
         }
     }
@@ -111,7 +128,7 @@ public class Bomber extends Character {
             case RIGHT -> this.setVelocity(defaultVel, 0);
             case UP -> this.setVelocity(0, -defaultVel);
             case DOWN -> this.setVelocity(0, defaultVel);
-            case PLACEBOMB -> placeBombAt(getTileX(), getTileY());
+            case PLACEBOMB -> placeBombAt(pixelX, pixelY);
         }
         if (direction != NONE && direction != PLACEBOMB) {
             currentAnimate = animation.get(direction);
